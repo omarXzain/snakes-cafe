adb0833e752de1ab25318187c072e794e7a683be
Omar Zain <omaren2010@gmail.com>
2020-12-01 02:36:05 +0200

    fixed

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,55 +1,60 @@
 print("""
-    **************************************
-    **    Welcome to the Snakes Cafe!   **
-    **    Please see our menu below.    **
-    **
-    ** To quit at any time, type "quit" **
-    **************************************
-    Appetizers
-    ----------
-    Wings
-    Cookies
-    Spring Rolls
-
-    Entrees
-    -------
-    Salmon
-    Steak
-    Meat Tornado
-    A Literal Garden
-
-    Desserts
-    --------
-    Ice Cream
-    Cake
-    Pie
-    
-    Drinks
-    ------
-    Coffee
-    Tea
-    Unicorn Tears
-    ***********************************
-    ** What would you like to order? **
-    ***********************************
-    """
-      )
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+***********************************
+** What would you like to order? **
+***********************************
+""")
 
 
-meal = {}
+meal = []
+
+menu = ['Wings', 'Cookies', 'Spring Rolls', 'Salmon', 'Steak', 'Meat Tornado',
+        'A Literal Garden', 'Ice Cream', 'Cake', 'Pie', 'Coffee', 'Tea', 'Unicorn Tears']
 
 
 def pickFood():
     order = ""
-    while order != "quit":
-        order = input('>')
-        if order in meal:
-            meal[order] += 1
+
+    while True:
+        order = input('> ').capitalize()
+        if order == 'quit':
+            break
+
+        if order in menu:
+            meal.append(order)
+            counter = meal.count(order)
+            if order != "quit":
+                print(
+                    f"** {counter} order of {order} have been added to your meal")
         else:
-            meal[order] = 1
-        if order != "quit":
-            print(
-                f"** {meal[order]} order of {order} have been added to your meal")
+            if order not in menu:
+                print('Please choose from the menu above!')
 
 
 if __name__ == '__main__':
